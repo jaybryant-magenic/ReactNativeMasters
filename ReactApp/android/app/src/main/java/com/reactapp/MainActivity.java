23616337afd7f630e76c9bc79a9cef98d7183391
Jay Bryant Casilang <jayc@magenic.com>
2018-09-19 17:30:13 +0800

    Setup Image Picker.

--- ReactApp/android/app/src/main/java/com/reactapp/MainActivity.java
+++ ReactApp/android/app/src/main/java/com/reactapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.reactapp;
 
+import android.support.annotation.NonNull;
+
 import com.facebook.react.ReactActivity;
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback;
+import com.facebook.react.modules.core.PermissionListener;
 
-public class MainActivity extends ReactActivity {
-
+public class MainActivity extends ReactActivity implements OnImagePickerPermissionsCallback {
+    private PermissionListener listener;
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -12,4 +16,21 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "ReactApp";
     }
+
+    @Override
+    public void setPermissionListener(@NonNull PermissionListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (listener != null)
+        {
+            listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
 }
